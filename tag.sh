2aa87af1a22f509cd4e5779dc684fd39660ede31
learnforpractice <learnforpractice@gmail.com>
2023-06-16 10:51:25 +0800

    Bump version to v2.0.2

--- tag.sh
+++ tag.sh
@@ -1,6 +1,6 @@
-VERSION=v2.0.1
+VERSION=v2.0.2
 TARGET=fork
 git push $TARGET :refs/tags/$VERSION
 git tag -d $VERSION
-git tag $VERSION -m $VERSION
+git tag $VERSION -F release.txt
 git push -f $TARGET $VERSION
